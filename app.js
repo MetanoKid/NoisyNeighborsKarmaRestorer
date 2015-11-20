04402e0c775e25ff56849621c04e22fc301defea
MetanoKid <carloscdcskate@gmail.com>
2015-11-21 00:10:13 +0100

    Adds express and tests it working
    Simple test for basic routing

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+// dependencies
+var express = require("express");
+
+// systems we'll be using
+var app = express();
+
+// routing
+app.get("/", function (request, response) {
+	response.send("Everything's working!");
+});
+
+// start server
+var server = app.listen(1337, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("NoisyNeighborsKarmaRestorer listening at http://%s:%s", host, port);
+});
